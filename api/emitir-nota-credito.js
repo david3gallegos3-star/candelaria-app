645116cade901d771dff0c61d3cbac31e7f792fa
David Candelaria 1 <david3gallegos3@gmail.com>
2026-05-22 21:36:27 -0500

    fix: clampear fecha documento modificado a hoy si viene adelantada por UTC

--- api/emitir-nota-credito.js
+++ api/emitir-nota-credito.js
@@ -73,7 +73,9 @@ module.exports = async function handler(req, res) {
 
     tipo_documento_modificado:          '01',
     numero_documento_modificado:        numero_factura,
-    fecha_emision_documento_modificado: fecha_emision_factura || fechaHoy,
+    fecha_emision_documento_modificado: (fecha_emision_factura && fecha_emision_factura <= fechaHoy)
+      ? fecha_emision_factura
+      : fechaHoy,
 
     motivo,
 
